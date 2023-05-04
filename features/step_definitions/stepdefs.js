5586f771a5fe506a32d64b12acfc23fbd84bddc2
Laineywang <laineywang@trendforce.com>
2023-05-04 18:18:14 +0800

    init

---
+++ features/step_definitions/stepdefs.js
@@ -0,0 +1,26 @@
+const assert = require("assert");
+const { Given, When, Then } = require("@cucumber/cucumber");
+
+function isItFriday(today) {
+  if (today === "Friday") {
+    return "TGIF";
+  } else {
+    return "Nope";
+  }
+}
+
+Given("today is Friday", function () {
+  this.today = "Friday";
+});
+
+Given("today is Sunday", function () {
+  this.today = "Sunday";
+});
+
+When("I ask whether it's Friday yet", function () {
+  this.actualAnswer = isItFriday(this.today);
+});
+
+Then("I should be told {string}", function (expectedAnswer) {
+  assert.strictEqual(this.actualAnswer, expectedAnswer);
+});
